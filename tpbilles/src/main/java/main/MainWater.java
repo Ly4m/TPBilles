e90d3127fc206864ee5ad908afd13472fead2604
piorunj <jordan.piorun@gmail.com>
2016-01-26 20:49:15 +0100

    graph

--- tpbilles/src/main/java/main/MainWater.java
+++ tpbilles/src/main/java/main/MainWater.java
@@ -7,7 +7,6 @@ import java.util.List;
 import core.Environnement;
 import core.SMA;
 import core.billes.Directions;
-import core.wator.Mur;
 import core.wator.Nemo;
 import core.wator.Shark;
 import javafx.animation.KeyFrame;
@@ -17,10 +16,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -28,18 +27,18 @@ import tools.Randomizer;
 
 public class MainWater extends Application {
 
-	private static int largeur = 100;
-	private static int hauteur = 100;
-	private static int nbShark = 80;
-	private static int nbNemo = 600;
+	private static int largeur = 200;
+	private static int hauteur = 200;
+	private static int nbShark = 1000;
+	private static int nbNemo = 2500;
 	private static int tempsAttente = 120;
 	private static int tempsArret = 0;
 	private static final int SIZE = 5;
 	private static long seed = Calendar.getInstance().getTimeInMillis();
 
-	public static List<Circle> Circle;
+	public static List<Circle> circle;
 
-	public static ObservableList<Circle> CircleObs;
+	public static ObservableList<Circle> circleObs;
 	public static Pane canvas;
 
 	public static void main(String[] args) {
@@ -49,9 +48,9 @@ public class MainWater extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		Circle = new ArrayList<Circle>();
+		circle = new ArrayList<Circle>();
 
-		CircleObs = FXCollections.observableArrayList(Circle);
+		circleObs = FXCollections.observableArrayList(circle);
 
 		Randomizer.setSeed(seed);
 		final Environnement env = new Environnement(largeur, hauteur);
@@ -59,13 +58,19 @@ public class MainWater extends Application {
 		boolean ok = false;
 
 		canvas = new Pane();
-		final Scene scene = new Scene(canvas, largeur * 5, hauteur * 5);
+		final Scene scene = new Scene(canvas, largeur, hauteur);
 
+		// for(int i = SIZE; i < hauteur; i += SIZE ){
+		// canvas.getChildren().addAll(new Line(0, i, largeur, i));
+		// }
+		// for(int i = SIZE; i < largeur; i += SIZE ){
+		// canvas.getChildren().addAll(new Line(i, 0, i, hauteur));
+		// }
 		primaryStage.setTitle("Chase me");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
-//		 Ajout des requins
+		// Ajout des requins
 		for (int i = 0; i < nbShark; i++) {
 			ok = false;
 			while (!ok) {
@@ -80,7 +85,7 @@ public class MainWater extends Application {
 
 					ok = true;
 
-					CircleObs.add(shark.getCircle());
+					circleObs.add(shark.getCircle());
 				} catch (IllegalArgumentException ignore) {
 				}
 			}
@@ -100,22 +105,21 @@ public class MainWater extends Application {
 					sma.addAgent(nemo);
 					ok = true;
 
-					CircleObs.add(nemo.getCircle());
+					circleObs.add(nemo.getCircle());
 				} catch (IllegalArgumentException ignore) {
 				}
 			}
 
 		}
 
-		canvas.getChildren().addAll(CircleObs);
+		canvas.getChildren().addAll(circleObs);
 
 		final long start = Calendar.getInstance().getTimeInMillis();
 		final long stop = tempsArret * 1000;
 		int nbTours = 0;
 		double tempsTotalRun = 0;
 
-
-		final Timeline loop = new Timeline(new KeyFrame(Duration.millis(30), new EventHandler<ActionEvent>() {
+		final Timeline loop = new Timeline(new KeyFrame(Duration.millis(33), new EventHandler<ActionEvent>() {
 
 			public void handle(final ActionEvent t) {
 
@@ -125,6 +129,21 @@ public class MainWater extends Application {
 		}));
 
 		loop.setCycleCount(Timeline.INDEFINITE);
+
+		PieChart.Data sharkData = new PieChart.Data("shark", 40);
+		PieChart.Data nemoData = new PieChart.Data("nemo", 60);
+
+		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(sharkData, nemoData);
+		final PieChart chart = new PieChart(pieChartData);
+
+		chart.setTitle("Proportion");
+
+		((Group) scene.getRoot()).getChildren().add(chart);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		pieChartData.get(1).setPieValue(Math.random() * 100);
 		loop.play();
 	}
 
